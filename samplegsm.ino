1c51cce39144c8a66be8b08f7f8cbc684fcfc816
JITHINREJI <30714442+JITHINREJI@users.noreply.github.com>
2017-08-04 13:59:57 +0530

    setting gsm module
    our project aims to prevent drunken drive and if its detected the owner of the vehicle will get notified via message send through gsm module. The program attached enables the GSM module to send message to the registered mobile number when the alcohol detecting sensor gives an output

---
+++ samplegsm.ino
@@ -0,0 +1,37 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+
+#include <SoftwareSerial.h>
+void setup()
+{
+  mySerial.begin(9600);   
+  Serial.begin(9600);  
+  delay(100);
+}
+
+
+void loop()
+{
+  int x=analogRead(A0);
+  if (Serial.available()>0)
+  if (x>370)
+  {
+     SendMessage();
+  }   
+    
+  }
+ void SendMessage()
+{
+  mySerial.println("AT+CMGF=1");   
+ delay(1000);  
+  mySerial.println("AT+CMGS=\"+917403900273\"\r"); 
+  delay(1000);
+  mySerial.println("Drunken driving detected");
+  delay(100);
+   mySerial.println((char)26);
+  delay(1000);
+}
+
+
+
