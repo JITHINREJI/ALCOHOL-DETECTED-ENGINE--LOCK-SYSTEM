a426b78907dd090706f9e6a46910ccd4242ddc6b
JITHINREJI <30714442+JITHINREJI@users.noreply.github.com>
2017-08-05 08:36:29 +0530

    ALCOHOL DETECTOR
    Full program code for the alcohol detector with GSM module

---
+++ final_gsm.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(9, 10);
+
+void setup()
+{
+   pinMode(13,OUTPUT);
+   mySerial.begin(9600);  
+   Serial.begin(9600);    
+   delay(100);
+}
+void loop()
+{
+  
+   int x=analogRead(A0);
+   Serial.print(x);
+   Serial.print("\n");
+   if(x>450)
+   {
+  
+         digitalWrite(13,HIGH);
+         if (Serial.available()>0)
+         mySerial.println("AT+CMGF=1");    
+         delay(100);
+         mySerial.println("AT+CMGS=\"+918547660715\"\r");
+         delay(100);
+         mySerial.println("Drunken Drive Detected");
+         delay(100);
+         mySerial.println((char)26);
+         delay(100);
+         mySerial.println("ATD+918547660715;");
+         delay(100);
+   }
+   else
+       digitalWrite(13,LOW);
+   }
+
